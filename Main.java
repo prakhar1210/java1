50510d91a4380b000d0ea3abaf8f3995c382f8ac
Prakhar Shrivastava <129577833+prakhar1210@users.noreply.github.com>
2024-01-18 12:17:34 +0530

    for loop

--- Main.java
+++ Main.java
@@ -1,14 +1,19 @@
 public class Main {
     public static void main(String args[]) {
 
-        int a = 20;
-        int b = 15;
-        int sum = a + b;
-        int multiply = a * b;
-        double div = a / b;
-        System.out.println("sum = " + sum);
-        System.out.println(multiply);
-        System.out.println(div);
+        // int a = 20;
+        // int b = 15;
+        // int sum = a + b;
+        // int multiply = a * b;
+        // double div = a / b;
+        // System.out.println("sum = " + sum);
+        // System.out.println(multiply);
+        // System.out.println(div);
+
+        int a = 10;
+        for (int i = 1; i <= a; i++) {
+            System.out.print(i);
+        }
 
     }
 }
