184e8950f48925ced5272a6285d2991bdcc6829a
Prakhar Shrivastava <129577833+prakhar1210@users.noreply.github.com>
2024-01-18 11:39:39 +0530

    java vars and sum

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String args[]) {
+
+        int a = 20;
+        int b = 15;
+        int sum = a + b;
+        int multiply = a * b;
+        double div = a / b;
+        System.out.println("sum = " + sum);
+        System.out.println(multiply);
+        System.out.println(div);
+
+    }
+}
